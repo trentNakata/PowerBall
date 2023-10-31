ef8f91ca72d52761bcf90d96cefa3c514e7169a4
trentNakata <78619753+trentNakata@users.noreply.github.com>
2023-10-31 22:14:48 +0000

    push

---
+++ powerball.js
@@ -0,0 +1,26 @@
+// server.js
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+//Set the response HTTP header with HTTP status and Content type
+res.statusCode = 200;
+res.setHeader("Content-Type", "text/html");
+
+for (let i = 0; i < 5; i++)
+{
+    let number = ((Math.floor(Math.random() * 69)+1)).toString();
+   res.write(number);
+   res.write(" ");
+}
+
+let number = ((Math.floor(Math.random() * 26)+1)).toString();
+res.write(number);
+
+res.end();
+});
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
